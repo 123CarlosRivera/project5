29dc6f0e68e6f0f96fb88f520e1300ae9c7bb4b3
crivera92 <79378321+crivera92@users.noreply.github.com>
2021-10-01 17:26:31 -0400

    pushing all changes made to project

--- carlos/app.py
+++ carlos/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pickle
 import numpy as np
 from PIL import Image
-
+from sklearn.preprocessing import StandardScaler
 # Header image
 col1, col2, col3 = st.beta_columns(3)
 image1 = Image.open('/Users/crivera/Desktop/project5/assets/US_SentencingCom_Symbol.JPG')
@@ -12,24 +12,23 @@ image2 = Image.open('/Users/crivera/Desktop/project5/assets/US_SentencingCom_Sym
 col2.image(image2,use_column_width=True)
 image3 = Image.open('/Users/crivera/Desktop/project5/assets/US_SentencingCom_Symbol.JPG')
 col3.image(image3,use_column_width=True)
-st.title('Judge Analyzer By GA Data Engineers')
+st.title('=Judge Analyzer By GA Data Engineers=')
 
 # Sidebar setup
 page = st.sidebar.selectbox('Select a category:',
 ('About','Predict'))
 
 if page=='About':
-    st.write("This will be a short paragraph of the purpose of this app and project")
-
+    st.write("This app was designed for use by anyone representing a person convicted of a criminal offense. Please enter the relevant information into the fields on the site; the app will then predict whether the defendant will receive prison time. This predictive tool can be used to assess the likelihood that a case was misclassified or mishandled, thereby helping lawyers focus on appealing those cases that fall into these categories.")
+    st.write("Behind the scenes, this application predicts whether a defendant will receive a prison sentence or alternative sentence using a variety of models trained on data from the United States Sentencing Commission taken in the fiscal year 2020. Each model gets one “vote” in determining the final prediction.")
 if page=='Predict':
 
     # user inputs
     disposit = st.text_input("Enter diposition of defendants case:* (ex. 0-5)")
-    drugmin = st.text_input("Offense mandatory minimum?:* (ex. In Months) ")
     mweight = st.text_input("Amount of Marijuana in grams:* (ex. Grams)")
     numdepen = st.text_input("Number of family dependants:* (ex. 1,2,3)")
     statmax = st.text_input("Max months in prison for offense:* (ex. 1,2,3)")
-    statmin = st.text_input("Minimum amount of months in prison for offense:* (ex. 1,2,3)")
+    #statmin = st.text_input("Minimum amount of months in prison for offense:* (ex. 1,2,3)")
     sources = st.text_input("Strength of evidence:* (ex. 1-3 or 5-9)")
 
 
@@ -43,7 +42,7 @@ if page=='Predict':
     offguide = 9 # offense guidline, 9 is for drug possession
     quarter = 4 # July 1 thru Sept 30
     casetype = 2 # Misdemeanor A
-    crimhist = 0 # no criminal History
+    crimhist = 0 # No criminal History
     combdrg2 = 4 # we're only working with marijuana cases
     dsplea = 3
     reas1 = 1
@@ -63,7 +62,7 @@ if page=='Predict':
             bag = pickle.load(pickle_in)
 
     # condition to make sure all required input is received
-    if disposit == "" or drugmin == "" or mweight == "" or numdepen == "" or sources == "" or statmax== "" or statmin== "":
+    if disposit == "" or mweight == "" or numdepen == "" or sources == "" or statmax== "": #or statmin== "":
         st.write("*Please answer all questions!!*")
 
     else:
@@ -71,11 +70,11 @@ if page=='Predict':
         X = [[
                         accgdln, casetype, combdrg2,
                         crimhist, int(disposit), district,
-                        int(drugmin), dsplea, intdum,
+                        dsplea, intdum,
                         methmin, int(mweight),nodrug,
                         offguide,quarter, reas1,
                         reas2, reas3, int(numdepen),
-                        int(sources), int(statmax), int(statmin)
+                        int(sources), int(statmax), #int(statmin)
         ]]
 
         # Do we need to Scale the inputs?
@@ -89,12 +88,14 @@ if page=='Predict':
 
 
 
+        ss = StandardScaler()
+        X_ss = ss.fit_transform(X)
 
-        knn_vote = knn.predict(X)
-        dt_vote = dt.predict(X)
-        et_vote = et.predict(X)
-        rf_vote = rf.predict(X)
-        bag_vote = bag.predict(X)
+        knn_vote = knn.predict(X_ss)
+        dt_vote = dt.predict(X_ss)
+        et_vote = et.predict(X_ss)
+        rf_vote = rf.predict(X_ss)
+        bag_vote = bag.predict(X_ss)
 
         model_votes = [knn_vote,dt_vote,et_vote,rf_vote,bag_vote]
         vote_1 = 0
@@ -102,11 +103,10 @@ if page=='Predict':
             if vote == 1:
                 vote_1+=1
             else:
-                continue
+                pass
 
         # if the vote count is 3 or greater, than the classification is prison time
         if vote_1>=3:
-            st.write(f"{dt.predict_proba(X)[0][1]*100}% chance of receiving a prison sentence.")
-
+            st.write(f"{dt.predict_proba(X)[0][1]*100}% chance of receiving a prison sentence after conviction.")
         else:
-            st.write(f"{dt.predict(X)}")
+            st.write(f"{dt.predict_proba(X)[0][0]*100}% chance of receiving a non-prison sentence after conviction!")
